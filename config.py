3da85ccf9b1736c89210fdc5269b8bdf90c96234
moazizaa <90508507+moazizaa@users.noreply.github.com>
2023-11-05 16:54:33 +0200

    Update config.py
    Signed-off-by: moazizaa <90508507+moazizaa@users.noreply.github.com>

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/moazizaa/a7a.git",
+    "https://github.com/moazizaa/AnonXMusic.git",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
