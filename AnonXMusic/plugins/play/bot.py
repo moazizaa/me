09136ef7b50ed3df25d1b038fe98ba93fa3157eb
moazizaa <90508507+moazizaa@users.noreply.github.com>
2024-02-01 21:01:21 +0200

    Add files via upload

--- AnonXMusic/plugins/play/bot.py
+++ AnonXMusic/plugins/play/bot.py
@@ -1,11 +1,12 @@
-
 import asyncio
-from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton 
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram import filters, Client
 from AnonXMusic import app
-from config import OWNER_ID
 
-@app.on_message(filters.command(['✨بوت','بوت'], prefixes=""))
+# Replace the following line with your actual OWNER_ID
+OWNER_ID = 123456789
+
+@app.on_message(filters.command(['بوت'], prefixes=""))
 async def Italymusic(client: Client, message: Message):
     me = await client.get_me()
     bot_username = me.username
@@ -17,21 +18,18 @@ async def Italymusic(client: Client, message: Message):
     chat_id = message.chat.id
     try:
         member = await client.get_chat_member(chat_id, user_id)
-        if user_id == 1183747742:
-             rank = "⋆ المبرمج مصطفي العزايري..🙂♥️"
+        if user_id == 5089553588:
+             rank = "يالهوي ده مالك السورس بنفسو ياعيال في البار😱⚡️"
         elif user_id == OWNER_ID:
-             rank = "⋆ صاحب البوت ومطوري حبيبي..🙂♥️"
+             rank = "مـالك الـبوت العظمه 🫡⚡️"
         elif member.status == 'creator':
-             rank = "⋆ صاحب الجروب حبيب الكل..🙂♥️"
+             rank = "مـالك الـبـار 🫡⚡️"
         elif member.status == 'administrator':
-             rank = "⋆ مشرف الجروب الغالي..🙂♥️"
+             rank = "مـشـرف الـبـار🫡⚡️"
         else:
              rank = "لاسف انت عضو فقير🥺💔"
     except Exception as e:
         print(e)
         rank = "مش عرفنلو مله ده😒"
-    async for photo in client.get_chat_photos("me", limit=1):
-
-                    await message.reply_photo(photo.file_id,       caption=f"""⋆ اسمي الكيوت ⇇: {bot_name} 🥹♥️""", reply_markup=keyboard)
-
-
+        await message.reply_text(
+        text=f"""نعم حبيبي : {italy} 🥰❤️\n**انا اسمي القميل : {bot_name} 🥺🙈\n**رتبتك هي : {rank}""", reply_markup=keyboard)
